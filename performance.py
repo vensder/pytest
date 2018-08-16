2c66b8a0d47c1a9b3252a22887446ed6a2264990
Dmitrii Makarov <dmitrii_makarov@epam.com>
2018-08-16 15:44:00 +0300

    added performance.py

---
+++ performance.py
@@ -0,0 +1,31 @@
+import time
+import string
+
+
+def search_for_elm(L,e):
+    for i in L:
+        if i == e:
+            return True
+    return False
+
+
+L = list(string.ascii_lowercase)[:-1]
+
+
+for l in ['a', 'z']:
+    t = time.process_time()
+
+    for i in range(10000):
+        S = search_for_elm(L, l)
+        
+    elapsed_time = time.process_time() -t
+    print(round(elapsed_time, 6), S, l)
+
+
+
+    t = time.process_time()
+    for i in range(10000):
+        S = l in L
+        
+    elapsed_time = time.process_time() -t
+    print(round(elapsed_time, 6), S, l)
